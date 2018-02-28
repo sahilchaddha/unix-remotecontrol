e28f488aa4aff8a11c2b0412356785315c2c08ab
Sahil Chaddha <sahilchaddha@gmail.com>
2018-02-28 15:43:34 +0800

    updated dummy token

--- src/environment.js
+++ src/environment.js
@@ -1,8 +1,8 @@
 var env = {
     port: '3000',
-    pass: 'uncharted',
+    pass: 'lol',
     logLevel: 'info',
-    sessionToken: 'abcd'
+    sessionToken: 'f64f2940-fae4-11e7-8c5f-ef356f279131'
 }
 
 module.exports = env
