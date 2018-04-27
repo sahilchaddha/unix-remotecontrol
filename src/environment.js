721273eb5f420af2a329a67d382d8e96354aa0bd
Sahil Chaddha <sahilchaddha@gmail.com>
2018-04-27 22:32:35 +0800

    Updated env

--- src/environment.js
+++ src/environment.js
@@ -2,7 +2,7 @@ var env = {
     port: '3000',
     pass: 'uncharted',
     logLevel: 'info',
-    sessionToken: 'abcd',
+    sessionToken: 'f64f2940-fae4-11e7-8c5f-ef356f279131',
     youtubePlaylistUrl: 'qpgTC9MDx1o&list=RDMMqpgTC9MDx1o',
     itunesPlaylist: 'MyPlaylist'
 }
